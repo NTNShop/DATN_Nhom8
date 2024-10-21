d7387d26040ca62c18a6f3353e7fda9d511f90d5
Maihaohao <maihaohao0704907392@gmail.com>
2024-10-21 16:39:22 +0700

    [FIX GIAO DIỆN TRANG BÀI VIẾT]

--- datn/src/component/client/blog/index.js
+++ datn/src/component/client/blog/index.js
@@ -1,6 +1,6 @@
 // src/Contact.js
 import React from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import Header from "../../../component/client/home/header";
 import Footer from "../../../component/client/home/footer";
 import Cart from "../../../assets/img/cart/cart.png";
@@ -11,77 +11,135 @@ const Blog = () => {
     <>
       <Header />
       <div>
-        <img src={banner} alt="Logo"/>
+        <img src={banner} alt="Logo" />
       </div>
       <div className="container">
-      <div className="row">
-        <div className="col-lg-12 text-center">
-          <div className="breadcrumb__text">
-            <h2 className="text-dark pt-5" style={{ borderBottom: '2px solid #de0000'}}>BÀI VIẾT</h2>
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <div className="breadcrumb__text">
+              <h2
+                className="text-dark pt-5"
+                style={{ borderBottom: "2px solid #de0000" }}
+              >
+                BÀI VIẾT
+              </h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
       <div className="blog spad text-center">
         <div className="container">
           <div className="row">
             <div className="col-lg-4 col-md-5">
-              <div className="">
-                <div className="blog__sidebar__search">
-                  <form action="#">
-                    <input type="text" placeholder="Tìm kiếm xe máy..." />
-                    <button type="submit">
+              <div className="p-4 bg-light shadow rounded">
+                <div className="blog__sidebar__search mb-4">
+                  <form action="#" className="d-flex">
+                    <input
+                      type="text"
+                      className="form-control me-2"
+                      placeholder="Tìm kiếm xe đạp..."
+                    />
+                    <button type="submit" className="btn btn-dark">
                       <span className="icon_search"></span>
                     </button>
                   </form>
                 </div>
 
-
-                <div className="blog__sidebar__item">
-                  <h4>Tin tức xe máy gần đây</h4>
+                <div className="blog__sidebar__item mb-4">
+                  <h4 className="mb-3">Tin tức xe đạp gần đây</h4>
                   <div className="blog__sidebar__recent">
-                    <a href="#" className="blog__sidebar__recent__item">
+                    <a
+                      href="#"
+                      className="blog__sidebar__recent__item d-flex mb-3"
+                    >
                       <div className="blog__sidebar__recent__item__pic">
-                        <img src={Cart1} width={100}  alt="" />
+                        <img
+                          src={Cart1}
+                          width={100}
+                          className="rounded img-thumbnail"
+                          alt="Xe đạp địa hình"
+                        />
                       </div>
-                      <div className="blog__sidebar__recent__item__text">
-                        <h6>Top 5 xe máy tay ga tốt nhất năm 2024</h6>
-                        <span>NGÀY 05 THÁNG 03, 2024</span>
+                      <div className="blog__sidebar__recent__item__text ms-3">
+                        <h6 className="text-dark">
+                          Top 5 xe đạp địa hình tốt nhất năm 2024
+                        </h6>
+                        <span className="text-muted">
+                          NGÀY 05 THÁNG 03, 2024
+                        </span>
                       </div>
                     </a>
-                    <a href="#" className="blog__sidebar__recent__item">
+                    <a
+                      href="#"
+                      className="blog__sidebar__recent__item d-flex mb-3"
+                    >
                       <div className="blog__sidebar__recent__item__pic">
-                        <img src={Cart1}  width={100}  alt="" />
+                        <img
+                          src={Cart1}
+                          width={100}
+                          className="rounded img-thumbnail"
+                          alt="Xe đạp điện"
+                        />
                       </div>
-                      <div className="blog__sidebar__recent__item__text">
-                        <h6>Các mẫu xe điện mới nhất trên thị trường</h6>
-                        <span>NGÀY 05 THÁNG 03, 2024</span>
+                      <div className="blog__sidebar__recent__item__text ms-3">
+                        <h6 className="text-dark">
+                          Các mẫu xe đạp điện mới nhất trên thị trường
+                        </h6>
+                        <span className="text-muted">
+                          NGÀY 05 THÁNG 03, 2024
+                        </span>
                       </div>
                     </a>
-                    <a href="#" className="blog__sidebar__recent__item">
+                    <a
+                      href="#"
+                      className="blog__sidebar__recent__item d-flex mb-3"
+                    >
                       <div className="blog__sidebar__recent__item__pic">
-                        <img src={Cart1}  width={100} alt="" />
+                        <img
+                          src={Cart1}
+                          width={100}
+                          className="rounded img-thumbnail"
+                          alt="Xe đạp đua"
+                        />
                       </div>
-                      <div className="blog__sidebar__recent__item__text">
-                        <h6>Đánh giá xe phân khối lớn mới ra mắt</h6>
-                        <span>NGÀY 05 THÁNG 03, 2024</span>
+                      <div className="blog__sidebar__recent__item__text ms-3">
+                        <h6 className="text-dark">
+                          Đánh giá các mẫu xe đạp đua mới ra mắt
+                        </h6>
+                        <span className="text-muted">
+                          NGÀY 05 THÁNG 03, 2024
+                        </span>
                       </div>
                     </a>
                   </div>
                 </div>
+
                 <div className="blog__sidebar__item">
-                  <h4>Tìm kiếm theo</h4>
-                  <div className="blog__sidebar__item__tags">
-                    <a href="#">Xe tay ga</a>
-                    <a href="#">Xe côn tay</a>
-                    <a href="#">Xe điện</a>
-                    <a href="#">Xe phân khối lớn</a>
-                    <a href="#">Tin tức xe máy</a>
-                    <a href="#">Đánh giá</a>
+                  <h4 className="mb-3">Tìm kiếm theo</h4>
+                  <div className="blog__sidebar__item__tags d-flex flex-wrap">
+                    <a href="#" className="btn btn-outline-dark btn-sm m-1">
+                      Xe đạp địa hình
+                    </a>
+                    <a href="#" className="btn btn-outline-dark btn-sm m-1">
+                      Xe đạp đua
+                    </a>
+                    <a href="#" className="btn btn-outline-dark btn-sm m-1">
+                      Xe đạp điện
+                    </a>
+                    <a href="#" className="btn btn-outline-dark btn-sm m-1">
+                      Xe đạp gấp
+                    </a>
+                    <a href="#" className="btn btn-outline-dark btn-sm m-1">
+                      Phụ kiện xe đạp
+                    </a>
+                    <a href="#" className="btn btn-outline-dark btn-sm m-1">
+                      Tin tức xe đạp
+                    </a>
                   </div>
                 </div>
               </div>
             </div>
+
             <div className="col-lg-8 col-md-7">
               <div className="row">
                 <div className="col-lg-6 col-md-6 col-sm-6">
@@ -100,10 +158,13 @@ const Blog = () => {
                         </li>
                       </ul>
                       <h5>
-                        <Link to="/blog-details/1">Xe tay ga phù hợp với nhu cầu của bạn</Link>
+                        <Link to="/blog-details/1">
+                          Xe tay ga phù hợp với nhu cầu của bạn
+                        </Link>
                       </h5>
                       <p>
-                        Khám phá những mẫu xe tay ga đang hot trên thị trường và tìm cho mình một chiếc xe phù hợp.
+                        Khám phá những mẫu xe tay ga đang hot trên thị trường và
+                        tìm cho mình một chiếc xe phù hợp.
                       </p>
                       <Link to="/blog-details/1" className="blog__btn">
                         ĐỌC THÊM <span className="arrow_right"></span>
@@ -128,10 +189,13 @@ const Blog = () => {
                         </li>
                       </ul>
                       <h5>
-                        <Link to="/blog-details/2">Hướng dẫn chọn xe điện tiết kiệm</Link>
+                        <Link to="/blog-details/2">
+                          Hướng dẫn chọn xe điện tiết kiệm
+                        </Link>
                       </h5>
                       <p>
-                        Những lưu ý quan trọng khi chọn mua xe điện để tiết kiệm chi phí và bảo vệ môi trường.
+                        Những lưu ý quan trọng khi chọn mua xe điện để tiết kiệm
+                        chi phí và bảo vệ môi trường.
                       </p>
                       <Link to="/blog-details/2" className="blog__btn">
                         ĐỌC THÊM <span className="arrow_right"></span>
@@ -155,10 +219,13 @@ const Blog = () => {
                         </li>
                       </ul>
                       <h5>
-                        <Link to="/blog-details/2">Hướng dẫn chọn xe điện tiết kiệm</Link>
+                        <Link to="/blog-details/2">
+                          Hướng dẫn chọn xe điện tiết kiệm
+                        </Link>
                       </h5>
                       <p>
-                        Những lưu ý quan trọng khi chọn mua xe điện để tiết kiệm chi phí và bảo vệ môi trường.
+                        Những lưu ý quan trọng khi chọn mua xe điện để tiết kiệm
+                        chi phí và bảo vệ môi trường.
                       </p>
                       <Link to="/blog-details/2" className="blog__btn">
                         ĐỌC THÊM <span className="arrow_right"></span>
@@ -182,10 +249,13 @@ const Blog = () => {
                         </li>
                       </ul>
                       <h5>
-                        <Link to="/blog-details/2">Hướng dẫn chọn xe điện tiết kiệm</Link>
+                        <Link to="/blog-details/2">
+                          Hướng dẫn chọn xe điện tiết kiệm
+                        </Link>
                       </h5>
                       <p>
-                        Những lưu ý quan trọng khi chọn mua xe điện để tiết kiệm chi phí và bảo vệ môi trường.
+                        Những lưu ý quan trọng khi chọn mua xe điện để tiết kiệm
+                        chi phí và bảo vệ môi trường.
                       </p>
                       <Link to="/blog-details/2" className="blog__btn">
                         ĐỌC THÊM <span className="arrow_right"></span>
