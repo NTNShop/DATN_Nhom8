b5ffdda6b1a9e63890034abf38af25df7088c735
Ngô Nhân <108146054+NTNShop@users.noreply.github.com>
2024-10-23 22:25:21 +0700

    up

--- datn/src/component/client/home/header.js
+++ datn/src/component/client/home/header.js
@@ -20,8 +20,8 @@ const Header = () => {
               <div className="col-lg-6 col-md-6">
                 <div className="header__top__left">
                   <ul>
-                    <li><i className="fa fa-envelope"></i> hello@colorlib.com</li>
-                    <li>Free Shipping for all Order of $99</li>
+                    <li><i className="fa fa-envelope"></i> nhom8@gmail.com</li>
+                    <li>Miễn phí ship cho tất cả hóa đơn từ 50 triệu</li>
                   </ul>
                 </div>
               </div>
@@ -35,15 +35,15 @@ const Header = () => {
                   </div>
                   <div className="header__top__right__language">
                     <i className="bi bi-globe-americas"></i>
-                    <div className="pl-2">English</div>
+                    <div className="pl-2">Tiếng Việt</div>
                     <span className="arrow_carrot-down"></span>
                     <ul>
-                      <li><a href="#">Spanish</a></li>
-                      <li><a href="#">English</a></li>
+                      <li><a href="#">Tiếng Việt</a></li>
+                      <li><a href="#">Tiếng Anh</a></li>
                     </ul>
                   </div>
                   <div className="header__top__right__auth">
-                    <a href="/login"><i className="fa fa-user"></i> Login</a>
+                    <a href="/login"><i className="fa fa-user"></i> Đăng nhập</a>
                   </div>
                 </div>
               </div>
@@ -109,7 +109,7 @@ const Header = () => {
                     </a>
                   </li>
                   <li>
-                    <a href="#" className="pl-1">
+                    <a href="/favourites" className="pl-1">
                       <i className="fa fa-heart"></i>
                     </a>
                   </li>
@@ -145,8 +145,8 @@ const Header = () => {
               <div className="hero__search">
                 <div className="hero__search__form">
                   <form action="#">
-                    <input type="text" placeholder="What do you need?" />
-                    <button type="submit" className="site-btn">SEARCH</button>
+                    <input type="text" placeholder="Bạn muốn mua gì?" />
+                    <button type="submit" className="site-btn">Tìm kiếm</button>
                   </form>
                 </div>
                 <div className="hero__search__phone">
@@ -154,8 +154,8 @@ const Header = () => {
                     <i className="fa fa-phone"></i>
                   </div>
                   <div className="hero__search__phone__text">
-                    <h5>+65 11.188.888</h5>
-                    <span>support 24/7 time</span>
+                    <h5>0123456789</h5>
+                    <span>Hỗ trợ 24/7</span>
                   </div>
                 </div>
               </div>
